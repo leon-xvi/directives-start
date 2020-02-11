ac2be619d31cbb7ece2d937a334467eb0faec675
Justin Walker <walkerjt93@gmail.com>
2020-02-11 11:44:51 -0500

    used hostbinding to bind ot the style/background color of the element instead of using the renderer

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -4,12 +4,14 @@ import {
   HostListener,
   OnInit,
   Renderer2,
+  HostBinding,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
@@ -20,18 +22,20 @@ export class BetterHighlightDirective implements OnInit {
     // );
   }
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      'blue'
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    this.backgroundColor = 'blue';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      'transparent'
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    // );
+    this.backgroundColor = 'transparent';
   }
 }
